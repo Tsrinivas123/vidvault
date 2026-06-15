c44dea9acd32e9c07a7834fdf58053db6c049fdb
Tushar shrivas <tusharshrivas7999@gmail.com>
2026-06-15 14:41:26 +0530

    Fix Render Port

--- backend/app.py
+++ backend/app.py
@@ -469,6 +469,11 @@ def cleanup_loop():
 cleanup_thread = threading.Thread(target=cleanup_loop, daemon=True)
 cleanup_thread.start()
 
+# if __name__ == '__main__':
+#     # Run server locally on port 5000
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+
 if __name__ == '__main__':
-    # Run server locally on port 5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
